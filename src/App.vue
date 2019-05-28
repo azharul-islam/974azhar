b914023f452ac33674fdd29a1d0bf06f8c13ab67
Azharul Islam <azharulislam@Azharuls-MacBook-Pro.local>
2019-05-28 04:21:20 +0300

    klsahfd

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <div id="brand">
+        <span class="moonstone-blue">974</span>AZHAR
+      </div>
+      <router-link to="/">Home </router-link>
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
@@ -13,17 +16,68 @@
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: #294e69;
 }
 #nav {
   padding: 30px;
   a {
-    font-weight: bold;
-    color: #2c3e50;
+    font-family: 'DisposableDroid BB';
+    color: #294e69;
     &.router-link-exact-active {
-      color: #42b983;
+      color: #E04C55;
+
     }
   }
 }
+
+#brand {
+  font-family: 'DisposableDroid BB';
+  font-size: 50px;
+}
+
+.moonstone-blue {
+  color: #5BAFBD
+}
+
+a {
+
+  &.router-link-exact-active {
+    color: #E04C55;
+  }
+  &:hover{color: #E04C55;}
+}
+
+@font-face {
+  font-family: 'DisposableDroid BB_Bold';
+  src: url('/assets/fonts/DisposableDroidBB-Bold.woff2') format('woff2'),
+  url('/assets/fonts/DisposableDroidBB-Bold.woff') format('woff');
+  font-weight: bold;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: 'DisposableDroid BB_Italic';
+  src: url('/assets/fonts/DisposableDroidBB-Italic.woff2') format('woff2'),
+  url('/assets/fonts/DisposableDroidBB-Italic.woff') format('woff');
+  font-weight: normal;
+  font-style: italic;
+}
+
+@font-face {
+  font-family: 'DisposableDroid BB_BoldItalic';
+  src: url('/assets/fonts/DisposableDroidBB-BoldItalic.woff2') format('woff2'),
+  url('/assets/fonts/DisposableDroidBB-BoldItalic.woff') format('woff');
+  font-weight: bold;
+  font-style: italic;
+}
+
+@font-face {
+  font-family: 'DisposableDroid BB';
+  src: url('/assets/fonts/DisposableDroidBB.woff2') format('woff2'),
+  url('/assets/fonts/DisposableDroidBB.woff') format('woff');
+  font-weight: normal;
+  font-style: normal;
+}
+
+
 </style>
